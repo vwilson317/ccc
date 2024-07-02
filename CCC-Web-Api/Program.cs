8e6c514e0d4fd8b2f2386ab0f2f335c97d6de139
vincent <vincent.j.wilson317@gmail.com>
2024-07-02 09:01:34 -0300

    fixed bug with tailwind added some of the class structure

--- CCC-Web-Api/Program.cs
+++ CCC-Web-Api/Program.cs
@@ -18,28 +18,56 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
+var mockData = new Barraca[]
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    new Barraca{Name = "81"},
+    new Barraca{Name = "Rasta"}, 
+    new Barraca{Name = "89"}, 
+    new Barraca{Name = "Made up"}, 
+    new Barraca{Name = "Green"}
 };
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/barracas", () =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    return mockData;
 })
-.WithName("GetWeatherForecast");
+.WithName("Barracas");
 
 app.Run();
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+public class Barraca {
+    public int Id{get;set;}
+    public string Name{get;set;}
+    public string LogoUrl{get;set;}
+}
+
+public class Menu{
+    public int Id{get;set;}
+    public int BarracaId{get;set;}
+    public DateTime LastUpdateUtc{get;set;}
+}
+
+public class MenuItem {
+    public int Id{get;set;}
+    public int MenuId{get;set;}
+    public double Cost{get;set;} //reals
+    public string ImageUrl {get;set;}
+}
+
+public class Bill {
+    public int Id{get;set;}
+    public string ExternalId{get;set;}
+    public string QRCode{get;set;}
+    public IList<BillItem> Items {get;set;}
+    public DateTime DateUtc{get;set;}
+}
+
+public class BillItem {
+    public int Qty{get;set;}
+    public int MenuItemId {get;set;}
 }
